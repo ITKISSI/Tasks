af7e3247edc4391ec4dcfd6d34b288fe752ddd03
Abdelaziz Kissi <azizkissi29@gmail.com>
2021-05-07 04:26:13 +0100

    commiting 2nd file

---
+++ Tasks.sql
@@ -0,0 +1,8 @@
+use fadel
+
+/* 1st task */ 
+/* select email,count(*) as 'count' from utilisateur group by email having count(*)>1 */ 
+
+
+/* 2nd task */ 
+select email,pw from utilisateur where email like 'fadel@gmail.com'
